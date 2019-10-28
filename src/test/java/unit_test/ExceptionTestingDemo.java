f2acb21cc0b58176e361af231ab0c50c24330993
ounihamza100 <hamza.ellouni@gmail.com>
2019-10-28 16:09:14 +0100

    tests

--- src/test/java/unit_test/ExceptionTestingDemo.java
+++ src/test/java/unit_test/ExceptionTestingDemo.java
@@ -1,12 +1,35 @@
 package unit_test;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * @author Hamza Ouni
  */
 public class ExceptionTestingDemo {
+
+
+    @Test
+    public void oldSchoolApproach() {
+
+        try {
+            Integer.parseInt("foo");
+            fail("NumberFormatException Expected");
+        } catch (NumberFormatException expected) {
+            assertEquals("For input string: \"foo\"",expected.getMessage());
+        }
+
+    }
+
+
+    @org.junit.Test(expected = NumberFormatException.class)
+    public void annotationParameterApproach() { Integer.parseInt("foo"); }
+
+
+    //*****************************************************************
 
 
     @org.junit.jupiter.api.Test
